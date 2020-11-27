2782c36ab64b9e6d01d1a37eb5216fa1e1860d55
Rainbow-of-Dawn <as.linkpro@gmail.com>
2020-11-27 12:17:18 +0100

    Add what was required of Quest 5

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -7,15 +7,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/program", name="program_")
+ */
 class ProgramController extends AbstractController
 {
     /**
-     * @Route("/programs/", name="program_index")
+     * @Route("/", name="program_index")
      */
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Séries',
         ]);
+    }
+
+    /**
+     * @Route("/{id}", requirements={"id"="\d+"}, methods={"GET"}, name="program_show")
+     */
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', ['id' => $id]);
     }
 }
